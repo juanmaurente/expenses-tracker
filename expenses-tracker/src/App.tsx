61f8db68e823e92d3dc2728665342c457c87f9ca
Juan Maurente <juanmaurente@gmail.com>
2023-03-22 11:40:22 +1100

    Built ExpenseList and delete button functionality

--- expenses-tracker/src/App.tsx
+++ expenses-tracker/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
+import ExpenseList from './expense-tracker/components/ExpenseList';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [expenses, setExpenses] = useState([
+		{
+			id: 1,
+			description: 'Groceries',
+			amount: 50.0,
+			category: 'Food',
+		},
+		{
+			id: 2,
+			description: 'Gasoline',
+			amount: 30.0,
+			category: 'Transportation',
+		},
+		{
+			id: 3,
+			description: 'Movie tickets',
+			amount: 20.0,
+			category: 'Entertainment',
+		},
+		{
+			id: 4,
+			description: 'Restaurant',
+			amount: 75.0,
+			category: 'Food',
+		},
+		{
+			id: 5,
+			description: 'Gym membership',
+			amount: 60.0,
+			category: 'Health',
+		},
+	]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div>
+			<ExpenseList
+				expenses={expenses}
+				onDelete={(id) =>
+					setExpenses(expenses.filter((e) => e.id !== id))
+				}
+			/>
+		</div>
+	);
 }
 
-export default App
+export default App;
